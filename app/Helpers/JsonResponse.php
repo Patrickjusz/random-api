e678c7d58a079b8e92d034f500e28e93dd5a98cf
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-19 19:05:15 +0100

    Add setHttpCode method in JsonResponse

--- app/Helpers/JsonResponse.php
+++ app/Helpers/JsonResponse.php
@@ -6,13 +6,20 @@ use App\Helpers\ResponseInterface;
 
 class JsonResponse implements ResponseInterface
 {
-    public function __construct(array $data)
+    public function __construct(array $data, int $httpCode = 200)
     {
-        $this->getHeaders();
+        $this->setHeaders();
+        $this->setHttpCode($httpCode);
         echo json_encode($data);
     }
 
-    private function getHeaders() {
+    private function setHeaders()
+    {
         header('Content-Type: application/json; charset=utf-8');
+    }
+
+    private function setHttpCode($httpCode): int
+    {
+        return (int)http_response_code($httpCode);
     }
 }
