b1b55b5679b9985d9faacb24816e5e26b73a39ae
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-19 19:27:00 +0100

    Add URL sanitizer in config

--- config/api.php
+++ config/api.php
@@ -4,7 +4,7 @@ define('DATABASE_TYPE', 'sqlite3');
 define('DATABASE_HOST_OR_FILE',  __DIR__ . '/../data/app.db');
 
 //CATCH THE REQUEST
-define('REQUEST_URL', $_GET['route'] ?? '');
+define('REQUEST_URL', filter_var(rtrim($_GET['route'] ?? '', '/'), FILTER_SANITIZE_URL));
 define('REQUEST_PARAM', $_POST ?? []);
 define('REQUEST_HEADERS', getallheaders() ?? []);
 
